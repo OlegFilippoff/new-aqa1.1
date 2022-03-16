842e61b79e6bb79abe80c5649188f233d53e6d1f
Oleg Filippov <5157175@bk.ru>
2022-03-16 16:44:05 +0300

    api annotation added

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -9,49 +9,49 @@ public class CashbackHackerTest {
 
     CashbackHacker service = new CashbackHacker();
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void shouldReturn1000whenPurchase0() {
         int expected = 1000;
         int actual = service.remain(0);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void shouldReturn999whenPurchase1() {
         int expected = 999;
         int actual = service.remain(1);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void shouldReturn500whenPurchase500() {
         int expected = 500;
         int actual = service.remain(500);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void shouldReturn1whenPurchase999() {
         int expected = 1;
         int actual = service.remain(999);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void shouldReturnLimitWhenPurchaseMoreLimit() {
         int expected = 1000;
         int actual = service.remain(3000);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void shouldReturn0WhenPurchase1000() {
         int expected = 0;
         int actual = service.remain(1000);
         assertEquals(expected, actual);
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void shouldReturn0WhenPurchaseIsNegative() {
         int expected = 0;
         int actual = service.remain(-1);
