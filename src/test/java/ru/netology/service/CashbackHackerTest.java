db286ec3170d9a989b4b7a5ebd6ba7eed40b25ca
Oleg Filippov <5157175@bk.ru>
2022-03-16 16:33:23 +0300

    added junit4

--- src/test/java/ru/netology/service/CashbackHackerTest.java
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -1,8 +1,9 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CashbackHackerTest {
 
@@ -12,48 +13,48 @@ public class CashbackHackerTest {
     public void shouldReturn1000whenPurchase0() {
         int expected = 1000;
         int actual = service.remain(0);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn999whenPurchase1() {
         int expected = 999;
         int actual = service.remain(1);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn500whenPurchase500() {
         int expected = 500;
         int actual = service.remain(500);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn1whenPurchase999() {
         int expected = 1;
         int actual = service.remain(999);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturnLimitWhenPurchaseMoreLimit() {
         int expected = 1000;
         int actual = service.remain(3000);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn0WhenPurchase1000() {
         int expected = 0;
         int actual = service.remain(1000);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void shouldReturn0WhenPurchaseIsNegative() {
         int expected = 0;
         int actual = service.remain(-1);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
